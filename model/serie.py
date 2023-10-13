67362032fe9d5e04d85de752efee278e0cbc61ec
JakeHartmann <jake.r.hartmann@gmail.com>
2023-10-13 19:51:32 -0300

    apagar getter de temporadas

--- model/serie.py
+++ model/serie.py
@@ -3,11 +3,6 @@ from model.midia import Midia
 
 class Serie(Midia):
     def __init__(self, titulo: str):
-        self.__temporadas = []
+        self.temporadas = []
         if isinstance(titulo, str):
             super().__init__(titulo)
-
-    @property
-    def temporadas(self):
-        if self.__temporadas:
-            return self.__temporadas

e10baf6a82dab53318c41d26ca32da879ab4fba2
JakeHartmann <jake.r.hartmann@gmail.com>
2023-10-14 17:36:13 -0300

    modificar entidade Temporada

--- model/temporada.py
+++ model/temporada.py
@@ -1,5 +1,14 @@
 
 
 class Temporada():
-    def __init__(self):
+    def __init__(self, numero: int):
+        if isinstance(numero, int):
+            self.numero = numero
         self.episodios = []
+
+    # @property
+    # def numero(self):
+    #     return self.__numero
+    # @numero.setter
+    # def numero(self, numero):
+    #     self.__numero = numero

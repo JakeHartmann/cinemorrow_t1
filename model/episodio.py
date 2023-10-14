025f0ee19175b67bdc23d85497a2001f42918c2d
JakeHartmann <jake.r.hartmann@gmail.com>
2023-10-14 17:35:23 -0300

    modificar entidade Episodio

--- model/episodio.py
+++ model/episodio.py
@@ -1,15 +1,35 @@
 
 
 class Episodio():
-    def __init__(self, duracao: int):
-        if isinstance(duracao, int):
-            self.__duracao = duracao
+    def __init__(self, numero: int):
+        if isinstance(numero, int):
+            self.__numero = numero
+        # if isinstance(duracao, int):
+        #     self.__duracao = duracao
+        self.__assistido = False
 
     @property
-    def duracao(self):
-        return self.__duracao
+    def numero(self):
+        return self.__numero
+    @numero.setter
+    def numero(self, numero):
+        if isinstance(numero, int):
+            self.__numero = numero
 
-    @duracao.setter
-    def duracao(self, duracao: int):
-        if isinstance(duracao, int):
-            self.__duracao = duracao
+    # @property
+    # def duracao(self):
+    #     return self.__duracao
+    # @duracao.setter
+    # def duracao(self, duracao):
+    #     if isinstance(duracao, int):
+    #         self.__duracao = duracao
+
+    @property
+    def assistido(self):
+        return self.__assistido
+    @assistido.setter
+    def assistido(self, assistido):
+        if isinstance(assistido, bool):
+            self.__assistido = assistido
+        else:
+            print("O valor de assistido deve ser um booleano.")

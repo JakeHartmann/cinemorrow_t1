ba81c2dc847d696278687091a907b3d0d09654f8
JakeHartmann <jake.r.hartmann@gmail.com>
2023-10-16 17:25:16 -0300

    modificar TelaGrupo

--- view/tela_grupo.py
+++ view/tela_grupo.py
@@ -5,3 +5,22 @@ from view.abstract_tela import AbstractTela
 class TelaGrupo(AbstractTela):
     def __init__(self):
         pass
+
+    def recebe_input_hora_minutos(self, mensagem):
+        while True:
+            try:
+                entrada = input(mensagem)
+                partes = entrada.split(":")
+                if len(partes) == 2:
+                    hora = int(partes[0])
+                    minutos = int(partes[1])
+                    if 0 <= hora <= 23 and 0 <= minutos <= 59:
+                        return hora, minutos
+                    else:
+                            print("Por favor, insira uma hora válida (0-23) e minutos válidos (0-59).")
+                else:
+                    print("Formato inválido. Use HH:MM.")
+            except ValueError:
+                print("Entrada inválida. Use HH:MM.")
+
+    

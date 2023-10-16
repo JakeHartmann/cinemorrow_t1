b4158ac1d00ecb5440f878ce9c11414db53c771f
JakeHartmann <jake.r.hartmann@gmail.com>
2023-10-16 10:22:24 -0300

    resolver problema de importação

--- sistema_cinemorrow.py
+++ sistema_cinemorrow.py
@@ -1,5 +1,7 @@
 from controller.ctrl_principal import CtrlPrincipal
+import sys,os
 
+sys.path.insert(0,os.path.abspath(os.curdir))
 
 if __name__ == "__main__":
     ctrl_principal = CtrlPrincipal()

fff57ecab2168d52c86c37f4c9933f696cd7c5d2
JakeHartmann <jake.r.hartmann@gmail.com>
2023-10-16 10:31:45 -0300

    modificar CtrlGrupo

--- controller/ctrl_grupo.py
+++ controller/ctrl_grupo.py
@@ -1,19 +1,32 @@
-from controller.ctrl_principal import CtrlPrincipal
 from view.tela_grupo import TelaGrupo
 from model.grupo import Grupo
 from datetime import datetime
 
 
 class CtrlGrupo():
-    def __init__(self, ctrl_principal: CtrlPrincipal):
-        if isinstance(ctrl_principal, CtrlPrincipal):
-            self.__ctrl_principal = ctrl_principal
+    def __init__(self, ctrl_principal):
+        self.__ctrl_principal = ctrl_principal
         
         self.__tela_grupo = TelaGrupo()
         
         self.__grupos = []
     
-    def cria_grupo():
+    @property
+    def grupos(self):
+        return self.__grupos
+
+    def abre_tela(self):
+        self.__tela_grupo.mostra_opcoes()
+        
+        lista_opcoes = {1: self.cria_grupo, 2: self.remove_grupo,
+                        3: self.altera_grupo, 4: self.lista_grupos, 5: self.retorna}
+
+        while True:
+            opcao = self.__tela_grupo.recebe_input_int("Escolha uma opção (grupos): ", [1, 2, 3, 4, 5])
+            escolha = lista_opcoes[opcao]
+            escolha()
+
+    def cria_grupo(self):
         titulo = str(input("Digite o título do grupo: "))
         # to do: checar se existe pelo menos um objeto de pessoa já existente
         integrante = str(input("Digite o email do integrante base do grupo: "))
@@ -25,19 +38,30 @@ class CtrlGrupo():
         hora = int(input("Digite a hora da sessão do grupo: "))
         minutos = int(input("""Digite os minutos da sessão do grupo
                         (aperte enter caso seja uma hora em ponto): """))
-        return Grupo(titulo, integrante, midia_associada, datetime(ano, mes, dia, hora, minutos))    
+        self.__grupos.append(Grupo(titulo, integrante, midia_associada, datetime(ano, mes, dia, hora, minutos)))    
 
-    def remove_grupo():
+    def remove_grupo(self):
         pass
     
-    def modifica_grupo():
+    def altera_grupo(self):
         pass
     
-    def adiciona_membro():
+    def adiciona_membro(self):
         pass
     
-    def remove_membro():
+    def remove_membro(self):
         pass
     
-    def lista_grupos():
+    def lista_grupos(self):
         pass
+    
+    def imprimir_grupo(self, grupo: Grupo):
+        print(f"Nome do grupo: {grupo.titulo}")
+        print(f"Membros do grupo:")
+        for membro in grupo.pessoas:
+            print("- " + membro.nome)
+        print(f"Midia associada ao grupo: {grupo.midia_associada.titulo} ({type(grupo.midia_associada).__name__})")
+        print(f"Data da próxima sessão do grupo: {grupo.data.strftime('%Y-%m-%d')} às {grupo.data.strftime('%H:%M')}")
+    
+    def retorna(self):
+        self.__ctrl_principal.abre_tela()
